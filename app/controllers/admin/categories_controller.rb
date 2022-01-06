a52dd1807969fd8cacb0016c5424c67d070d4d58
Xihai Luo <xihai.luo01@gmail.com>
2022-01-06 22:04:40 +0000

    add create action to save new category in db

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,4 +6,13 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new({name: params[:category][:name]})
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 end
