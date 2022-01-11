2307d0c167c100d89e6c0635cf89ccfa4d177fe0
Xihai Luo <xihai.luo01@gmail.com>
2022-01-11 00:53:37 +0000

    refactor create action in sessions controller to use user model class method auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    if user
       session[:user_id] = user.id
       redirect_to '/'
-    else
-      redirect_to '/sessions/new'
+      return
     end
+    redirect_to '/sessions/new'
   end
 
   def destroy
