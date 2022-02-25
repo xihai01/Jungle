08abb91d97d040379feafb09c843c6f6ad981cd5
Xihai Luo <xihai.luo01@gmail.com>
2022-02-24 23:18:05 -0500

    secure checkout page so only registered users can place an order

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,16 @@
 class Admin::BaseController < ApplicationController
-  http_basic_authenticate_with name: Rails.configuration.auth[:username], password: Rails.configuration.auth[:password]
+  # before_action :authenticate
+ http_basic_authenticate_with name: Rails.configuration.auth[:username], password: Rails.configuration.auth[:password]
+
+
+=begin  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      if (username == Rails.configuration.auth[:username] && password == Rails.configuration.auth[:password])
+        true
+      else
+        redirect_to cart_path
+      end
+    end
+  end
+=end
 end
